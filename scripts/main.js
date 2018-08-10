8e9be90b60ce4f1297993e1320dd6ee4baade207
Eric Smith <e.smith.py@gmail.com>
2018-08-10 12:42:12 -0700

    removed script, added php to automatically show images
    summary explains EVERYTHING

--- scripts/main.js
+++
@@ -1,7 +0,0 @@
-document.getElementById("current_page").innerHTML = "Hello!"
-
-var nav_bar = $("#navbarNav a");
-
-nav_bar.on("click", function () {
-   document.getElementById("current_page").innerHTML = "Nice click!" // responds to a click by saying 'Nice click!'
-});

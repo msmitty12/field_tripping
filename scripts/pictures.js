e77468ea313ec5aed66001514d3a4f7b2da31d69
Soccertanker <e.smith.py@gmail.com>
2018-08-10 17:45:03 -0700

    trying

---
+++ scripts/pictures.js
@@ -0,0 +1,14 @@
+$(document).ready(function(){
+	var dir = "images/pictures/";
+
+	$.ajax ({
+		url : dir,
+		success : function (data) {
+			$(data).find("a").attr("href", function (i, val) {
+				if (val.match(/\.(jpeg|jpg|png|gif)%/) ) {
+					$("pictures").append( "<div class=\"col-sm-12 col-md-6 col-lg-12\"> <img src='" + dir + val + "'></div>" );
+				}
+			});
+		}
+	});
+});

02dd7612dc4af574b4fff3db0bc02f64525703d9
Soccertanker <e.smith.py@gmail.com>
2018-08-10 17:52:55 -0700

    testing

---
+++ scripts/test.js
@@ -0,0 +1,14 @@
+$(document).ready(function(){
+var folder = "../images/pictures/";
+
+$.ajax({
+    url : folder,
+    success: function (data) {
+        $(data).find("a").attr("href", function (i, val) {
+            if( val.match(/\.(jpe?g|png|gif)$/) ) { 
+                $("body").append( "<img src='"+ folder + val +"'>" );
+            } 
+        });
+    }
+});
+});

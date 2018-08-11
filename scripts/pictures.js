68ffea1aafbb7813d3ffe0177396f1b415784bd6
Soccertanker <e.smith.py@gmail.com>
2018-08-10 18:20:40 -0700

    ok

--- scripts/pictures.js
+++ scripts/pictures.js
@@ -1,12 +1,12 @@
 $(document).ready(function(){
-	var dir = "images/pictures/";
+	var dir = "../images/pictures/";
 
 	$.ajax ({
 		url : dir,
 		success : function (data) {
 			$(data).find("a").attr("href", function (i, val) {
 				if (val.match(/\.(jpeg|jpg|png|gif)%/) ) {
-					$("pictures").append( "<div class=\"col-sm-12 col-md-6 col-lg-12\"> <img src='" + dir + val + "'></div>" );
+					$("#pictures").append( "<div class=\"col-sm-12 col-md-6 col-lg-12\"> <img src='" + dir + val + "'></div>" );
 				}
 			});
 		}
--- scripts/test.js
+++ scripts/test.js
@@ -6,7 +6,7 @@ $.ajax({
     success: function (data) {
         $(data).find("a").attr("href", function (i, val) {
             if( val.match(/\.(jpe?g|png|gif)$/) ) { 
-                $("body").append( "<img src='"+ folder + val +"'>" );
+                $("#pictures").append( "<img src='"+ folder + val +"'>" );
             }
         });
     }
